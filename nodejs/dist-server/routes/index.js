f452a51beb70b1a8d88bc55e7f6c750f00a9a786
Martin Angelov <martin.angelov@uber.com>
2021-08-27 13:25:38 +0300

    Added Node.js option

---
+++ nodejs/dist-server/routes/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var router = _express["default"].Router();
+/* GET home page. */
+
+
+router.get('/', function (req, res, next) {
+  res.send('Hello World!');
+});
+var _default = router;
+exports["default"] = _default;
