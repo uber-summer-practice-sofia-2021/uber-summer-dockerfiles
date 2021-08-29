4b17aec11a6572545f7fc4f9fb3cb757338cc51c
Martin Angelov <martin.angelov@uber.com>
2021-08-29 21:15:22 +0300

    Added java and golang environments

---
+++ java/src/main/java/com/dev/server/DevServerApplication.java
@@ -0,0 +1,13 @@
+package com.dev.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevServerApplication.class, args);
+	}
+
+}
